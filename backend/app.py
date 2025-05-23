bc62b702c04cde080d5b83461f95a53501516d73
suscripciones2314 <suscripciones2314@gmail.com>
2025-05-23 22:59:42 +0200

    Ejercicio resuelto con todos los archivos

--- backend/app.py
+++ backend/app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "API operativa con código montado desde o host de JGA"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
